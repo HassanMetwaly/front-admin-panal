eddae86c26d85899dd1fad11c9045236bfc5d470
HASSAN METWALY <131253773+HassanMetwaly@users.noreply.github.com>
2023-08-22 22:45:37 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,26 @@
+const el = document.getElementById("wrapper");
+const toggleButton = document.getElementById("menu-toggle");
+toggleButton.onclick = function () {
+    el.classList.toggle("toggled");
+};
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+})()
